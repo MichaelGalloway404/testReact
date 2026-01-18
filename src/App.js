56c7b897ea962c1e26cc3af28a23244de85b7676
MichaelGallowayEOU <mgalloway1@eou.edu>
2026-01-18 16:15:06 -0600

    app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SecondPage from "./pages/SecondPage";
+import App from "./App";
 
 function App() {
   return (
