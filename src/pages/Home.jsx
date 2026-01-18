d6aca88c78d732855ae4c35c268a008895cc0813
MichaelGallowayEOU <mgalloway1@eou.edu>
2026-01-18 16:37:07 -0600

    path edit for bookSearch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import BookSearch from './components/BookSearch';
+import BookSearch from '../components/BookSearch';
 
 function Home() {
     const navigate = useNavigate();
