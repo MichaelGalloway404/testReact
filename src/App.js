ed4f2a79b30adaddd2e039b5669c58f394c0c5b1
MichaelGallowayEOU <mgalloway1@eou.edu>
2026-01-18 16:18:47 -0600

    remove app import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SecondPage from "./pages/SecondPage";
-import App from "./App";
 
 function App() {
   return (
